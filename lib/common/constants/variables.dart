80432aee3afbf423e619bb26603a828e685c62cb
medihermanto <meditinambunan27@gmail.com>
2023-10-09 16:35:56 +0800

    flutter_siakad

---
+++ lib/common/constants/variables.dart
@@ -0,0 +1,4 @@
+class Variables {
+  static const String baseUrl =
+      'https://7127-2001-448a-7069-2d8d-c575-6ccf-aacb-fe82.ngrok-free.app';
+}
---
+++ lib/pages/mahasiswa/setting_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_siakad/bloc/logout/logout_bloc.dart';
+import 'package:flutter_siakad/data/datasources/auto_local_datasource.dart';
+import 'package:flutter_siakad/pages/auth/auth_page.dart';
+
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: BlocProvider(
+        create: (context) => LogoutBloc(),
+        child: BlocConsumer<LogoutBloc, LogoutState>(
+          listener: (context, state) {
+            state.maybeWhen(
+              orElse: () {},
+              loaded: () {
+                AuthLocalDatasource().removeAuthData();
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
+                  return const AuthPage();
+                }));
+              },
+              error: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('logout error'),
+                  ),
+                );
+              },
+            );
+          },
+          builder: (context, state) {
+            return state.maybeWhen(
+              orElse: () {
+                return ElevatedButton(
+                  onPressed: () {
+                    context.read<LogoutBloc>().add(const LogoutEvent.logout());
+                  },
+                  child: const Text('Logout'),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
